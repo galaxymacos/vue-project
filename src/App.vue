e79c4509e5d372f766fab1d7c4d372357849bfeb
xunruan <xunruan@icloud.com>
2022-09-16 00:19:30 -0400

    Conditionals to toggle editing mode

--- src/App.vue
+++ src/App.vue
@@ -8,23 +8,31 @@
     {id: 3, label: "20 cups"},
     {id: 4, label: "10 plates"},
   ])
+  const editing = ref(false)
   const newItem = ref('')
   const newItemHighPriority = ref(false)
   const saveItem = ()=>{
     items.value.push({id: items.value.length + 1, label: newItem.value})
     newItem.value = ""
   }
+  const setEditingModel = (e) => {
+    editing.value = e
+  }
   </script>
   
   <template>
-    <h1>{{ header }}</h1>
+    <div class="header">
+      <h1>{{ header }}</h1>
+      <button class="btn btn-cancel" v-if="editing" @click="setEditingModel(false)">Cancel</button>
+      <button class="btn btn-primary" v-else @click="setEditingModel(true)">Add</button>
+    </div>
 <!--v-model.lazy can be used to update the value only when the input loses focus.-->
-    <form v-on:submit.prevent="saveItem()" class="add-item-form">
+    <form v-on:submit.prevent="saveItem()" class="add-item-form" v-if="editing">
       <input
           v-model.trim="newItem"
           type="text"
           placeholder="Add an item">
-      {{ newItem }} Priority:
+      Priority:
       <label><input type="checkbox" v-model="newItemHighPriority">High Priority</label>
       <br>
       <button class="btn btn-primary">Save Item</button>
@@ -35,4 +43,5 @@
         {{ label }}
       </li>
     </ul>
+    <p v-if="items.length === 0">Nothing to show</p>
   </template>
