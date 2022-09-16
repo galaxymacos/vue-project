012079c3d712f2b6676a682a4184cfd4723772ef
xunruan <xunruan@icloud.com>
2022-09-15 23:59:55 -0400

    User Events
    button click event,
    
    form submit event with prevent

--- src/App.vue
+++ src/App.vue
@@ -9,18 +9,22 @@
     {id: 4, label: "10 plates"},
   ])
   const newItem = ref('')
-  const newItemPriority = ref("low")
+  const newItemHighPriority = ref(false)
   </script>
   
   <template>
     <h1>{{ header }}</h1>
 <!--v-model.lazy can be used to update the value only when the input loses focus.-->
-    <input v-model.trim="newItem" type="text" placeholder="Add an item">
-    {{ newItem }} Priority:
-    <!-- two ways of adding label to the radio button -->
-    <label><input type="radio" v-model="newItemPriority" id="low" value="low">Low</label>
-    <input type="radio" v-model="newItemPriority" id="high" value="high"><label for="high">high</label>
-
+    <form v-on:submit.prevent="items.push({id: items.length + 1, label: newItem})" class="add-item-form">
+      <input
+          v-model.trim="newItem"
+          type="text"
+          placeholder="Add an item">
+      {{ newItem }} Priority:
+      <label><input type="checkbox" v-model="newItemHighPriority">High Priority</label>
+      <br>
+      <button class="btn btn-primary">Save Item</button>
+    </form>
     <ul>
 <!-- index in list is the index, in dictionary is the key-->
       <li v-for="({id, label}, index) in items" :key="id">
