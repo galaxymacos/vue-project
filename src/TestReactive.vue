4dce9db5569a69cbb4af0b2889c781c4feda56e7
xunruan <xunruan@icloud.com>
2022-09-16 14:17:23 -0400

    Reactivity Fundamentals

---
+++ src/TestReactive.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref, reactive } from 'vue'
+const count = ref(0)  // ref can be used to create a reactive variable (both object and primitive)
+const state = reactive({  // reactive only works on objects
+  count: 0
+})
+const increment = () => {
+  state.count++
+}
+</script>
+
+<template>
+  <button @click="increment">Increment count</button>
+  {{ state.count }}
+</template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import TestReactive from './TestReactive.vue'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+createApp(TestReactive).mount('#app')
