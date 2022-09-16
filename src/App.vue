fb0c9f6e2b4db2e9d1b15f678462d13ba34fac48
xunruan <xunruan@icloud.com>
2022-09-15 22:54:58 -0400

    radio buttons data binding
    v-model

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,18 @@
     {id: 4, label: "10 plates"},
   ])
   const newItem = ref('')
+  const newItemPriority = ref("low")
   </script>
   
   <template>
     <h1>{{ header }}</h1>
 <!--v-model.lazy can be used to update the value only when the input loses focus.-->
-    <input v-model.lazy="newItem" type="text" placeholder="Add an item">
-    {{ newItem }}
+    <input v-model.trim="newItem" type="text" placeholder="Add an item">
+    {{ newItem }} Priority:
+    <!-- two ways of adding label to the radio button -->
+    <label><input type="radio" v-model="newItemPriority" id="low" value="low">Low</label>
+    <input type="radio" v-model="newItemPriority" id="high" value="high"><label for="high">high</label>
+
     <ul>
 <!-- index in list is the index, in dictionary is the key-->
       <li v-for="({id, label}, index) in items" :key="id">
